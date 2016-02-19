6b2237db6eed651d3cbdc2e78c77d71c9be8f2e8
bogaczpi <bogaczpi@PC-B103-02.iut-valence.fr>
2016-02-19 14:28:33 +0100

    Main and AbaloneGame classes added.

---
+++ src/AbaloneGame.java
@@ -0,0 +1,15 @@
+
+public class AbaloneGame
+{
+	
+	
+	/**
+	 * Start an AbaloneGame previously created.
+	 */
+	public void start()
+    {
+	    // TODO Auto-generated method stub
+	    
+    }
+	
+}
